06b0ffb37944ec262aaab671b3e2c32936c1ff6e
MYEONGGI <smgsmg226@gmail.com>
2022-10-26 08:48:47 +0900

    10870번

--- 10870번.py
+++ 10870번.py
@@ -5,15 +5,13 @@
 # n이 주어졌을 때, n번째 피보나치 수를 구하는 프로그램을 작성하시오.
 
 def fibonacci(n):
-    k = 1
     
     if n == 0:
         return 0
     elif n == 1:
         return 1
     else:
-        for i in n:
-            k = i + fibonacci(n + 1)
-            return k
+        return fibonacci(n - 1) + fibonacci(n - 2)
         
-print(fibonacci(17))
+a = int(input())
+print(fibonacci(a))
